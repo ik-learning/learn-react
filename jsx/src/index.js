2f090b83b11c93144ea7de350fa9cc9d3c174ea7
Ivan Katliarchuk <ivan.katliarchuk@gmail.com>
2019-10-05 19:13:15 +0100

    get current time

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,9 +2,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const getButtonText = obj => {
+  return obj.text;
+}
+
+const getTime = _ => {
+  return (new Date()).toLocaleTimeString()
+}
 // Create react component
 const App = _ => {
-  return <div>Hi there!</div>
+  const buttonText = { text: 'click me!' };
+  const labelText = 'Enter name';
+  const style = { backgroundColor: 'blue', color: 'white' };
+  return (
+    <div>
+      <div>Current Time:</div>
+      <h3>{getTime()} PM</h3>
+      <label className="name" htlmfor="name">
+        {labelText}
+      </label>
+      <input id="name" type="text" />
+      <button style={style}>
+        {getButtonText(buttonText)}
+      </button>
+    </div>
+  );
 };
 // Take the react component and show it on the screen
 ReactDOM.render(
