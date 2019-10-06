01b44e2a6d59bc937ff04f37e8b9a164083942e8
Ivan Katliarchuk <ivan.katliarchuk@gmail.com>
2019-10-06 08:35:33 +0100

    create html component

--- components/src/index.js
+++ components/src/index.js
@@ -3,8 +3,23 @@ import ReactDOM from 'react-dom';
 
 const App = () => {
   return (
-    <div>
-      Hi, there!
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+        </div>
+        <div className="metadata">
+          <span className="date">Today at 6:00 PM</span>
+        </div>
+        <div className="text">
+          Nice blog post!
+        </div>
+      </div>
     </div>
   );
 };
