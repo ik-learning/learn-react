35d2413d60bdc22938847f7753031326546d7ad2
Ivan Katliarchuk <ivan.katliarchuk@gmail.com>
2019-10-06 12:46:38 +0100

    segment for simple div

--- components/src/index.js
+++ components/src/index.js
@@ -4,10 +4,17 @@ import faker from 'faker';
 
 import CommentDetail from './CommentDetail';
 import ApprovalCard from './ApprovalCard';
+import Segment from './Segment';
 
 const App = () => {
   return (
     <div className="ui container comments">
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
       <ApprovalCard>
         <CommentDetail
           author={faker.name.firstName()}
@@ -43,7 +50,15 @@ const App = () => {
           avatar={faker.image.avatar()}
           comment={faker.hacker.phrase()} />
       </ApprovalCard>
-
+      <Segment>
+        <div className="ui placeholder segment">
+          <div className="ui icon header">
+            <i className="pdf file outline icon"></i>
+            No documents are listed for this customer.
+                    </div>
+          <div className="ui primary button">Add Document</div>
+        </div>
+      </Segment>
     </div>
   );
 };
