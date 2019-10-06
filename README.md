711e49781b09bb04cad18ee4c1f05a7b1ed0282b
Ivan Katliarchuk <ivan.katliarchuk@gmail.com>
2019-10-06 10:02:50 +0100

    create approval card. embed childs

--- README.md
+++ README.md
@@ -7,5 +7,5 @@ The way to lern react
 
 ## CSS libraries
 
-- (Semantic UI)[https://semantic-ui.com/introduction/getting-started.html]
-- (Mini CSS)[https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css]
+- [Semantic UI](https://semantic-ui.com/introduction/getting-started.html)
+- [Mini CSS](https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css)
--- components/src/index.js
+++ components/src/index.js
@@ -3,35 +3,47 @@ import ReactDOM from 'react-dom';
 import faker from 'faker';
 
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 4:45PM"
-        avatar={faker.image.avatar()}
-        comment={faker.hacker.phrase()} />
-      <CommentDetail
-        author="Ivan"
-        timeAgo="Today at 2:15PM"
-        avatar={faker.image.avatar()}
-        comment={faker.hacker.phrase()} />
-      <CommentDetail
-        author="John"
-        timeAgo="Yesterday at 12:35PM"
-        avatar={faker.image.avatar()}
-        comment={faker.hacker.phrase()} />
-      <CommentDetail
-        author="Radka"
-        timeAgo="Monday at 5:00PM"
-        avatar={faker.image.avatar()}
-        comment={faker.hacker.phrase()} />
-      <CommentDetail
-        author="Kostya"
-        timeAgo="Friday at 6:05PM"
-        avatar={faker.image.avatar()}
-        comment={faker.hacker.phrase()} />
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          timeAgo="Today at 4:45PM"
+          avatar={faker.image.avatar()}
+          comment={faker.hacker.phrase()} />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          timeAgo="Today at 2:15PM"
+          avatar={faker.image.avatar()}
+          comment={faker.hacker.phrase()} />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          timeAgo="Yesterday at 12:35PM"
+          avatar={faker.image.avatar()}
+          comment={faker.hacker.phrase()} />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          timeAgo="Monday at 5:00PM"
+          avatar={faker.image.avatar()}
+          comment={faker.hacker.phrase()} />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          timeAgo="Friday at 6:05PM"
+          avatar={faker.image.avatar()}
+          comment={faker.hacker.phrase()} />
+      </ApprovalCard>
+
     </div>
   );
 };
