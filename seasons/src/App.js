0b5d617908db3dfac283713def43aada6a58593a
Ivan Katliarchuk <ivan.katliarchuk@gmail.com>
2019-10-06 13:37:00 +0100

    create first class object

--- seasons/src/App.js
+++ seasons/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const App = _ => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
-  return (
-    <div>
-      Hi, There!
-    </div>
-  );
-}
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
+    return (
+      <div>Latitude: </div>
+    );
+  };
+};
 
 export default App;
