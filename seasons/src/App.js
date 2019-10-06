2dda8f1d1fbeee24eb0296ce0162ef1e9f444064
Ivan Katliarchuk <ivan.katliarchuk@gmail.com>
2019-10-06 13:56:36 +0100

    set state

--- seasons/src/App.js
+++ seasons/src/App.js
@@ -4,15 +4,30 @@ class App extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      lat: null,
+      long: null
+    };
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        console.log(position);
+        this.setState({
+          lat: position.coords.latitude,
+          long: position.coords.longitude
+        });
+      },
+      err => console.log(err)
+    );
   }
 
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
-    );
     return (
-      <div>Latitude: </div>
+      <div>
+        <div>Latitude: {this.state.lat} </div>
+        <div>Longitute: {this.state.long} </div>
+      </div>
     );
   };
 };
