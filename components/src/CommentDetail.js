fdd271baa0e1acd3b1f15a6c8fed451fa6ca5564
Ivan Katliarchuk <ivan.katliarchuk@gmail.com>
2019-10-06 09:44:34 +0100

    properties in nutshell

--- components/src/CommentDetail.js
+++ components/src/CommentDetail.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import faker from 'faker';
 
 const CommentDetail = props => {
 
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" src={faker.image.avatar()} />
+        <img alt="avatar" src={props.avatar} />
       </a>
       <div className="content">
         <a href="/" className="author">
           {props.author}
-          </a>
+        </a>
       </div>
       <div className="metadata">
-        <span className="date">Today at 6:00 PM</span>
+        <span className="date">
+          {props.timeAgo}
+        </span>
       </div>
       <div className="text">
-        Not as great as previous!!!
-        </div>
+        {props.comment}
+      </div>
     </div>
   );
 };
--- components/src/index.js
+++ components/src/index.js
@@ -1,15 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
+
 import CommentDetail from './CommentDetail';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Ivan" />
-      <CommentDetail author="John" />
-      <CommentDetail author="Radka" />
-      <CommentDetail author="Kostya" />
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:45PM"
+        avatar={faker.image.avatar()}
+        comment={faker.hacker.phrase()} />
+      <CommentDetail
+        author="Ivan"
+        timeAgo="Today at 2:15PM"
+        avatar={faker.image.avatar()}
+        comment={faker.hacker.phrase()} />
+      <CommentDetail
+        author="John"
+        timeAgo="Yesterday at 12:35PM"
+        avatar={faker.image.avatar()}
+        comment={faker.hacker.phrase()} />
+      <CommentDetail
+        author="Radka"
+        timeAgo="Monday at 5:00PM"
+        avatar={faker.image.avatar()}
+        comment={faker.hacker.phrase()} />
+      <CommentDetail
+        author="Kostya"
+        timeAgo="Friday at 6:05PM"
+        avatar={faker.image.avatar()}
+        comment={faker.hacker.phrase()} />
     </div>
   );
 };
